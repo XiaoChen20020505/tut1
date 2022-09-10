e30844f48d69bba832f4187470b4747686000f56
XiaoChen20020505 <3387587002@qq.com>
2022-09-10 17:44:55 +0800

    Fixed bug 2.0

--- Greeter.java
+++ Greeter.java
@@ -20,7 +20,7 @@ public class Greeter {
     String language = args[0];
     String country = args[1];
     String name = args[2];
-    Greeter greeter = new Greeter(language, country, name);
+    Greeter greeter = new Greeter(language, country, newname);
     System.out.print(greeter.sayHello());
   }
 }
